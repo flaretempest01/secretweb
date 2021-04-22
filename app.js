2e07cba91196a567ed938fd000c0a11c6862589f
Kazuya <sato.kazuya01@gmail.com>
2021-04-22 15:56:56 +0900

    added bcrypt file and upgraded security

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
+const bcrypt = require("bcrypt");
 
+const saltRounds = 10;
 
 const app = express();
 
@@ -20,8 +23,6 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -38,35 +39,38 @@ app.get("/login", function(req, res){
 });
 
 app.post("/register", function(req, res){
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
 
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-  });
-
-  newUser.save(function(err){
-    if (!err){
-      res.render("secrets");
-    } else {
-      res.send(err);
-    }
+    newUser.save(function(err){
+      if (!err){
+        res.render("secrets");
+      } else {
+        res.send(err);
+      }
+    });
   });
 });
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = hash;
 
   User.findOne({email: username}, function(err, foundUser){
     if (err){
-      res.render(err);
-    } else {
-      if (foundUser) {
-        if (foundUser.password === password){
+        res.render(err);
+      } else {
+      bcrypt.compare(req.body.password, hash, function(err, result) {
+        if (result===true) {
+          if (foundUser.password === hash){
 
-          res.render("secrets");
+            res.render("secrets");
+          }
         }
-      }
+      });
     }
   });
 });
